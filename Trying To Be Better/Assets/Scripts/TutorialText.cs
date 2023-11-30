a06d07a8bb47b597b600267d8e73f267a32e62af
Lucas Fernando Soares Morgado de Souza <lucasmorgaadoo@gmail.com>
2023-11-29 23:53:51 -0300

    fade in/out on ui elements

--- Trying To Be Better/Assets/Scripts/TutorialText.cs
+++ Trying To Be Better/Assets/Scripts/TutorialText.cs
@@ -1,31 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TutorialText : MonoBehaviour
 {
-    private bool activated = false;
-    private CanvasGroup alpha;
+    private bool fadeIsComplete = true;
+    private TextMeshProUGUI _text;
 
     // Start is called before the first frame update
     void Awake()
     {
-        alpha = GetComponent<CanvasGroup>();
+        _text = GetComponent<TextMeshProUGUI>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && fadeIsComplete)
         {
-            //LeanTween.alphaCanvas(alpha, 1f, 1.5f);
+            StartCoroutine(FadeIn());
+            
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && fadeIsComplete)
         {
-            //LeanTween.alphaCanvas(alpha, 0f, 1.5f);
+            StartCoroutine(FadeOut());
         }
+    }
+
+    IEnumerator FadeIn ()
+    {
+        if (_text.color.a >= 0.9f)
+        {
+            yield break;
+        }
+
+        fadeIsComplete = false;
+
+        for (float i = 0; i <= 1f; i += Time.deltaTime)
+        {
+            _text.color = new Color(1, 1, 1, i);
+            yield return null;
+        } 
+
+        if (_text.color.a >= 0.9)
+        {
+            _text.color = new Color(1,1,1,1);
+        }
+
+        Debug.Log(_text.color.a);
+        fadeIsComplete = true;
+    }
+
+    IEnumerator FadeOut ()
+    {
+        if (_text.color.a <= 0.1f)
+        {
+            yield break; 
+        }
+
+        fadeIsComplete = false;
+
+        for (float i = 1; i >= 0f; i -= Time.deltaTime)
+        {
+            _text.color = new Color(1, 1, 1, i);
+            yield return null;
+        }
+
+        fadeIsComplete = true;
     }
 }
